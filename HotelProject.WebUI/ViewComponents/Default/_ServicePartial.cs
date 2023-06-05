7e158cf0bf3a33371042a8d92c1da52df230a498
M-racle <108946729+M-racle@users.noreply.github.com>
2023-06-05 13:52:17 +0300

    I made the Our Room section on the page dynamic. It's all in the database now. I also pull the services part from the database by making it dynamic.

--- HotelProject.WebUI/ViewComponents/Default/_ServicePartial.cs
+++ HotelProject.WebUI/ViewComponents/Default/_ServicePartial.cs
@@ -1,10 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.ServiceDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
 namespace HotelProject.WebUI.ViewComponents.Default
 {
     public class _ServicePartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ServicePartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var client = _httpClientFactory.CreateClient(); // Bir tane istemci oluşturduk.
+            var responseMessage = await client.GetAsync("http://localhost:35402/api/Service"); // Bu adrese istekte bullunuyoruz.
+            if (responseMessage.IsSuccessStatusCode)             //Eğer ki burası başarılı bir durum kodu dönerse
+            {
+                var jsonResult = await responseMessage.Content.ReadAsStringAsync();       //Gelen veriyi jsonResult a ata
+                var data = JsonConvert.DeserializeObject<List<ListServiceDto>>(jsonResult);    //Bize gelen data json türünde bu datayı deserilize edeceğiz  
+                var selectedData = data.Take(8).ToList();
+                return View(selectedData);
+            }
             return View();
         }
     }
